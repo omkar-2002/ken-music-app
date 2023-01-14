0dfefc7825cf3d82a9ccbe31facfd004cb364d42
omkar-2002 <omkarsalapurkar2002@gmail.com>
2023-01-14 18:33:10 +0530

    installed all the packages and did the setup

---
+++ kenmusicapp/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Hi</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
